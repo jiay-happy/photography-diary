355d94a85d26ad7ec00573ecbdce1fd0df3aa580
jiay-happy <2687586170@qq.com>
2026-06-08 17:05:45 +0800

    Update siteName in config.js

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  siteName: '龚仔的摄影日记',
+  siteName: '童的摄影日记',
   siteTitle: '记录生活，定格美好',
   siteDescription: '用镜头捕捉每一个值得珍藏的瞬间',
   footer: {
     social: [
       { label: '小红书', url: 'https://xhslink.com/m/8MA6NoHQesz', brand: 'xiaohongshu' },
       { label: '抖音', url: 'https://v.douyin.com/owyNGXrRXrA/', brand: 'douyin' },
-      { label: '微信：GongzVirAug' },
       { label: '邮箱：jogt@foxmail.com', url: 'mailto:jogt@foxmail.com' }
     ]
   }
